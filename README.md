ac367910a82911a57092a99937bb487431a5f995
vaanwd <vali29@gmail.com>
2012-10-04 19:58:02 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,43 @@
+# Sublime Text 2 Drush Package
+
+A simple package to run Drupal drush from Sublime Text editor.
+
+##Screenshots
+
+![Drush Insert Command](http://www.vaanwebdesign.ro/includes/images/sublime_drush_1.png)
+Open command bar with `ctrl`+`shift`+`d` (Windows)
+
+
+
+![Drush Results]
+(http://www.vaanwebdesign.ro/includes/images/sublime_drush_2.png)
+Drush results
+
+For a complete list of drush commands, please visit the [official site](http://drush.ws/).
+
+## Installation
+
+Easiest way to install the plugin is to use [Package Control](http://wbond.net/sublime_packages/package_control).
+
+Alternatively you can clone with git directly into `Packages` directory in the Sublime Text 2 application settings area. The directory name must be `Drush` without quotes.
+
+### Using Git
+
+Go to your Sublime Text 2 `Packages` directory and clone the repository using the command below:
+
+    git clone https://github.com/SublimeText/sublime_drush "Drush"
+
+### Download Manually
+
+* Download the files using the GitHub .zip download option
+* Unzip the files and rename the folder to `Drush`
+* Copy the folder to your Sublime Text 2 `Packages` directory
+
+
+
+
+
+[&copy; 2012 Vaan Web Design](http://www.vaanwebdesign.ro)
+
+
+
---
+++ drush.py
@@ -0,0 +1,63 @@
+import platform
+import sublime
+import sublime_plugin
+import os
+
+os.environ["TERM"] = "dumb"
+PLATFORM_IS_WINDOWS = platform.system() is 'Windows'
+
+class DrushCommand(sublime_plugin.TextCommand):
+  working_dirs = ''
+  path = ''
+  folders = []
+  DEFAULT_DRUSH_EXECUTABLE = 'drush.bat' if PLATFORM_IS_WINDOWS else 'drush'
+  DEFAULT_DRUSH_ARGS = ''
+  SETTINGS = sublime.load_settings("Drush.sublime-settings")
+  view_panel = None
+
+  def run(self, edit):
+    view = self.view
+    self.working_dirs = view.window().folders()
+    if len(self.working_dirs) > 0:
+      self.path = self.working_dirs[0]
+      self.view_panel = view.window().show_input_panel('drush', '', self.after_input, self.on_change, None)
+      self.view_panel.set_name('drush_command_bar')
+    else:
+      sublime.message_dialog('No opened project was found!\nPlease open a Drupal project!')
+
+  def after_input(self, text):
+    if text.strip() == "":
+      sublime.status_message("No command provided")
+      return
+    else:
+      self._runDrush(text)
+
+  def on_change(self, text):
+    if text.strip() == "":
+      return
+
+  def _runDrush(self, text):
+    try:
+      drush = str(self._get_drush_executable().strip())
+      drush_args = str(self._get_drush_sup_args().strip())
+      command_splitted = ''
+      import shlex
+      if drush_args != '':
+        command_splitted = [drush] + [drush_args] + shlex.split(str(text)) + [str('--root=' + self.path)]
+      else:
+        command_splitted = [drush] + shlex.split(str(text)) + [str('--root=' + self.path)]
+      sublime.active_window().run_command('exec', {'cmd': command_splitted, "working_dir": self.path})
+    except OSError as e:
+        error_message = 'Drush error: '
+        if e.errno is 2:
+            error_message += 'Could not find your "drush" executable.'
+        error_message += str(e)
+
+        sublime.status_message(error_message)
+        return ('', error_message)
+
+  def _get_drush_executable(self):
+    return self.SETTINGS.get('drush_executable') or self.DEFAULT_DRUSH_EXECUTABLE
+
+  def _get_drush_sup_args(self):
+    return self.SETTINGS.get('drush_args') or self.DEFAULT_DRUSH_ARGS
